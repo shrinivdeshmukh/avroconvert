f605a8e93d91b64ea82de338591ee85ef3c00859
shrinivas <shrinivas.deshmukh11@gmail.com>
2021-06-22 17:41:35 +0800

    read function parallelized

--- avroconvert/avroconvert.py
+++ avroconvert/avroconvert.py
@@ -74,8 +74,7 @@ class AvroConvert:
         if not bool(data):
             return None
         try:
-            logger.info('Converting bytes to avro')
-            logger.info(f'File {filename} in progress')
+            logger.info(f'[COVERTING] File {filename} bytes to avro')
             outfile = join(self.outfolder, self._change_file_extn(filename))
             avrodata = [r for r in reader(BytesIO(data))]
             logger.info(
